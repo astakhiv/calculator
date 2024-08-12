fc42467e7118de32f082971b4c20635fa7484e04
Arsen Stakhiv <arsen.stakhiv2305@gmail.com>
2024-08-12 21:40:09 +0300

    add keyboard support; refactor: add UIDecorator to handle UI interactions

--- main.js
+++ main.js
@@ -8,62 +8,90 @@ const negate = document.querySelector(".negate");
 const modulo = document.querySelector(".modulo");
 const dot = document.querySelector(".dot");
 
+const PrintNumberUI = UIDecorator(PrintNumber);
+const SelectOperationUI = UIDecorator(SelectOperation);
+const EvaluateUI = UIDecorator(Evaluate);
+const ModuloUI = UIDecorator(Modulo);
+const NegateUI = UIDecorator(Negate);
+const DeleteLastCharacterUI = UIDecorator(DeleteLastCharacter);
+const DotUI = UIDecorator(Dot);
+const ClearScreen = UIDecorator(ClearValues);
+
 let firstNumber = "";
 let operation = "";
 let secondNumber = "";
-
 let needClearOutput = false;
 
-numbers.forEach((num) => { num.addEventListener("click", (e => PrintNumber(e))); });
-operations.forEach((op) => { op.addEventListener("click", (e) => { SelectOperation(e) }); });
+numbers.forEach((num) => { num.addEventListener("click", (e => PrintNumberUI(e.target.textContent))); });
+operations.forEach((op) => { op.addEventListener("click", (e => SelectOperationUI(e.target.textContent))); });
 
-equals.addEventListener("click", Evaluate);
-
+equals.addEventListener("click", EvaluateUI);
 clear.addEventListener("click", ClearScreen);
-backspace.addEventListener("click", DeleteLastCharacter);
-negate.addEventListener("click", Negate);
-modulo.addEventListener("click", Modulo);
-dot.addEventListener("click", Dot);
+backspace.addEventListener("click", DeleteLastCharacterUI);
+negate.addEventListener("click", NegateUI);
+modulo.addEventListener("click", ModuloUI);
+dot.addEventListener("click", DotUI);
+document.addEventListener("keypress", KeyboardInput);
 
-function PrintNumber(e) {
-    const value = e.target.textContent;
-       
-    if (output.innerText === "0" || needClearOutput) {
-        output.innerText = "";
-        needClearOutput = false;
-    }
-
-    output.innerText += value; 
+function StringIsNumber(str) {
+    return isNaN(+str) === false;
 }
 
-function SelectOperation(e) {
-    operation = e.target.textContent;
+const funcForKey = {
+    "+": SelectOperationUI,
+    "-": SelectOperationUI,
+    "*": SelectOperationUI,
+    "/": SelectOperationUI,
+    "%": ModuloUI,
+    ".": DotUI,
+    "=": EvaluateUI,
+};
 
-    firstNumber = isNaN(+output.innerText) ? 0 : output.innerText;
+function KeyboardInput(e) {
+    if (StringIsNumber(e.key)) {
+        PrintNumberUI(e.key);
+    } else {
+        const func = funcForKey[e.key];
+        if (func === undefined) return;
+        func(e.key);
+    }
+
+}
+
+function PrintNumber(text, number) {
+    result = text + number;
+    
+    if (text === "0" || needClearOutput) {
+        result = number;
+        needClearOutput = false;
+    }
+    
+    return result;
+}
+
+function SelectOperation(text, symbol) {
+    operation = symbol;
+
+    firstNumber = isNaN(+text) ? 0 : text;
 
     needClearOutput = true;
 }
 
-function Evaluate() {
+function Evaluate(text) {
     if (operation === "") return;
-    secondNumber = output.innerText;
+    secondNumber = text;
 
     const result = Operate(+firstNumber, +secondNumber, operation);
 
-    output.innerText = result;
-    clearValues();
-   
+    ClearValues();
+    return result; 
 }
 
-function ClearScreen() {
-    output.innerText = "0";
-    clearValues();
-}
-
-function clearValues() {
+function ClearValues(text) {
     firstNumber = "";
     operation = "";
     secondNumber = "";
+    return "0";
 }
 
 function Operate(firstNumber, secondNumber, operator) {
@@ -82,59 +110,51 @@ function Operate(firstNumber, secondNumber, operator) {
 
 function Divide(a, b) {
     if (b === 0) {
+        needClearOutput = true;
         return "div by zero :(";
     }
     
     return Math.round((a / b) * 100) / 100;
 }
 
-function Dot() {
-    const text = output.innerText;
-
+function Dot(text) {
     for (let i = 0; i < text.length; i++) {
         if (text[i] === ".") {
             return;
         }
     }
 
-    output.innerText += ".";
+    return text + ".";
 }
 
-function Modulo() {
-    const text = output.innerText;
+function Modulo(text) {
+    if (isNaN(+text)) return;
 
-    if (isNaN(+text)) {
-        return;
-    }
-
-    output.innerText = text / 100;
-
+    return (+text / 100);
 }
 
-function Negate() {
-    const text = output.innerText;
-
-    let result = "";
-    if (text[0] === "-") {
-        result = text.slice(1);
-    } else if (text === "0") {
+function Negate(text) {
+    if (text === "0") {
         return;
+    } else if (text[0] === "-") {
+        return text.slice(1);
     } else {
-        result = "-" + text;        
+        return "-" + text;        
     }
-    
-    output.innerText = result;
 }
 
-function DeleteLastCharacter() {
-    const text = output.innerText;
-   
-    let result = "";
+function DeleteLastCharacter(text) {
     if (text.length === 1 || (text.length === 2 && text[0] === "-")) {
-        result = "0";
+        return "0";
     } else {
-        result = text.slice(0, -1);
+        return text.slice(0, -1);
     }
-    output.innerText = result
+}
 
+function UIDecorator(func) {
+    return function f(...args) {
+        const text = output.innerText;
+        const result = func(text, ...args);
+        output.innerText = result === undefined ? text : result;
+    }
 }
