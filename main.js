66f7da8a64b52e54a5feda97423dae843301704c
Arsen Stakhiv <arsen.stakhiv2305@gmail.com>
2024-08-12 17:12:49 +0300

    edit: moved logic to functions

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-function operate(firstNumber, secondNumber, operator) {
-    if (operator === "+") {
-        return (firstNumber + secondNumber);
-    } else if (operator === "-") {
-        return (firstNumber - secondNumber);
-    } else if (operator === "*") {
-        return (firstNumber * secondNumber);
-    } else if (operator === "/") {
-        return (secondNumber === 0) ? ("div by zero :(") :  (Math.round((firstNumber / secondNumber) * 100) / 100);
-    } else {
-        return 0;
-    }
-}
-
 const output = document.querySelector(".output");
 const numbers = document.querySelectorAll(".number");
 const operations = document.querySelectorAll(".operation");
@@ -26,66 +12,106 @@ let firstNumber = "";
 let operation = "";
 let secondNumber = "";
 
+let needClearOutput = false;
+
+numbers.forEach((num) => { num.addEventListener("click", (e => PrintNumber(e))); });
+operations.forEach((op) => { op.addEventListener("click", (e) => { SelectOperation(e) }); });
+
+equals.addEventListener("click", Evaluate);
+
+clear.addEventListener("click", ClearScreen);
+backspace.addEventListener("click", DeleteLastCharacter);
+negate.addEventListener("click", Negate);
+modulo.addEventListener("click", Modulo);
+dot.addEventListener("click", Dot);
+
+function PrintNumber(e) {
+    const value = e.target.textContent;
+       
+    if (output.innerText === "0" || needClearOutput) {
+        output.innerText = "";
+        needClearOutput = false;
+    }
+
+    output.innerText += value; 
+}
+
+function SelectOperation(e) {
+    operation = e.target.textContent;
+
+    firstNumber = isNaN(+output.innerText) ? 0 : output.innerText;
+
+    needClearOutput = true;
+}
+
+function Evaluate() {
+    if (operation === "") return;
+    secondNumber = output.innerText;
+
+    const result = Operate(+firstNumber, +secondNumber, operation);
+
+    output.innerText = result;
+    clearValues();
+   
+}
+
+function ClearScreen() {
+    output.innerText = "0";
+    clearValues();
+}
+
 function clearValues() {
     firstNumber = "";
     operation = "";
     secondNumber = "";
 }
 
-let needClearOutput = false;
-
-numbers.forEach((button) => {
-        button.addEventListener("click", (e) => {
-        const value = e.target.textContent;
-       
-        if (output.innerText === "0" || needClearOutput) {
-            output.innerText = "";
-            needClearOutput = false;
-        }
-
-        output.innerText += value;
-    });
-});
-
-operations.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        operation = e.target.textContent;
-    
-        firstNumber = isNaN(+output.innerText) ? 0 : output.innerText;
-
-        needClearOutput = true;
-    });
-});
-
-clear.addEventListener("click", () => {
-    output.innerText = "0";
-    firstNumber = "";
-    secondNumber = "";
-});
-
-equals.addEventListener("click", () => {
-    if (operation === "") return;
-    secondNumber = output.innerText;
-
-    const result = operate(+firstNumber, +secondNumber, operation);
-
-    clearValues();
-    output.innerText = result;
-});
-
-backspace.addEventListener("click", () => {
-    const text = output.innerText;
-   
-    let result = "";
-    if (text.length === 1 || (text.length === 2 && text[0] === "-")) {
-        result = "0";
+function Operate(firstNumber, secondNumber, operator) {
+    if (operator === "+") {
+        return (firstNumber + secondNumber);
+    } else if (operator === "-") {
+        return (firstNumber - secondNumber);
+    } else if (operator === "*") {
+        return (firstNumber * secondNumber);
+    } else if (operator === "/") {
+        return Divide(firstNumber, secondNumber);
     } else {
-        result = text.slice(0, -1);
+        return 0;
     }
-    output.innerText = result
-});
+}
 
-negate.addEventListener("click", () => {
+function Divide(a, b) {
+    if (b === 0) {
+        return "div by zero :(";
+    }
+    
+    return Math.round((a / b) * 100) / 100;
+}
+
+function Dot() {
+    const text = output.innerText;
+
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] === ".") {
+            return;
+        }
+    }
+
+    output.innerText += ".";
+}
+
+function Modulo() {
+    const text = output.innerText;
+
+    if (isNaN(+text)) {
+        return;
+    }
+
+    output.innerText = text / 100;
+
+}
+
+function Negate() {
     const text = output.innerText;
 
     let result = "";
@@ -98,26 +124,17 @@ negate.addEventListener("click", () => {
     }
     
     output.innerText = result;
-});
+}
 
-modulo.addEventListener("click", () => {
+function DeleteLastCharacter() {
     const text = output.innerText;
-
-    if (isNaN(+text)) {
-        return;
+   
+    let result = "";
+    if (text.length === 1 || (text.length === 2 && text[0] === "-")) {
+        result = "0";
+    } else {
+        result = text.slice(0, -1);
     }
+    output.innerText = result
 
-    output.innerText = text / 100;
-});
-
-dot.addEventListener("click", () => {
-    const text = output.innerText;
-
-    for (let i = 0; i < text.length; i++) {
-        if (text[i] === ".") {
-            return;
-        }
-    }
-
-    output.innerText += ".";
-});
+}
